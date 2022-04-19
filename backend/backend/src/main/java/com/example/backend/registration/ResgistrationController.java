56b140603bfd4eaf51ed21cc08caa9572fe195cf
SachPrecious <sachithrabandara49@gmail.com>
2022-04-19 06:58:50 +0530

    UserRegistration-RegistrationService

--- backend/backend/src/main/java/com/example/backend/registration/ResgistrationController.java
+++ backend/backend/src/main/java/com/example/backend/registration/ResgistrationController.java
@@ -2,6 +2,7 @@ package com.example.backend.registration;
 
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/registration")
 public class ResgistrationController {
 
+    private RegistrationService registrationService;
+    public String register(@RequestBody RegistrationRequest request){
+
+        return registrationService.register(request);
+    }
 
 }
