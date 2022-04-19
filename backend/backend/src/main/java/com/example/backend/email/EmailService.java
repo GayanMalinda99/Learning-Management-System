19965017dd04ac857f76be49b3f4a997c3b0b397
SachPrecious <sachithrabandara49@gmail.com>
2022-04-19 13:29:18 +0530

    Email Sender

--- backend/backend/src/main/java/com/example/backend/email/EmailService.java
+++ backend/backend/src/main/java/com/example/backend/email/EmailService.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 @AllArgsConstructor
@@ -18,6 +22,19 @@ public class EmailService implements EmailSender {
 
     @Override
     public void send(String to, String email) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper =
+                    new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email");
+            helper.setFrom("from.com");
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            LOGGER.error("failed to send email", e);
+            throw new IllegalStateException("failed to send email");
+        }
 
     }
 }
