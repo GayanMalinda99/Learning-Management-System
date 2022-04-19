a9375f22136c9a2e4375d6ecc53ad878af9c8737
SachPrecious <sachithrabandara49@gmail.com>
2022-04-19 08:10:40 +0530

    Email Validator Initial Commit

--- backend/backend/src/main/java/com/example/backend/registration/EmailValidator.java
+++ backend/backend/src/main/java/com/example/backend/registration/EmailValidator.java
@@ -1,10 +1,15 @@
 package com.example.backend.registration;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+
+@AllArgsConstructor
 @Service
 public class EmailValidator implements Predicate<String> {
+
+    private EmailValidator emailValidator;
 
     @Override
     public boolean test(String s) {
