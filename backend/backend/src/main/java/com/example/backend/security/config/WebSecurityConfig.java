555d2cf76839359c9ad6cf0c9dab8a611dcb9368
SachPrecious <sachithrabandara49@gmail.com>
2022-04-29 15:38:29 +0530

    resolved issues

--- backend/backend/src/main/java/com/example/backend/security/config/WebSecurityConfig.java
+++ backend/backend/src/main/java/com/example/backend/security/config/WebSecurityConfig.java
@@ -22,22 +22,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .csrf().disable()
-                    .authorizeRequests()
-                        .antMatchers("/api/v*/registration/**")
-                        .permitAll()
-                    .anyRequest().authenticated().and()
-                    .formLogin(form-> form
-                                   // .defaultSuccessUrl("/home")
-                             //       .loginPage("/login")
-                            .failureUrl("/login?error=true")
-                    );
-
-
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v*/registration/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .formLogin();
 
 
     }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
