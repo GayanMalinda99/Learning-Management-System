0e2cdaffbcd0e5ef40678bc528fbe6a07bf773ae
rragul <r.ragul0598@gmail.com>
2022-04-30 00:26:11 +0530

    update course

--- backend/backend/src/main/java/com/example/backend/course/model/Course.java
+++ backend/backend/src/main/java/com/example/backend/course/model/Course.java
@@ -1,10 +1,12 @@
 package com.example.backend.course.model;
 
+import com.example.backend.appuser.AppUser;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Getter
 @Setter
@@ -12,8 +14,9 @@ import javax.persistence.Id;
 public class Course {
 
     @Id
-    private String course_code;
+    private String code;
     private String course_name;
     private String course_description;
-
+    @OneToOne
+    private AppUser lecturer;
 }
