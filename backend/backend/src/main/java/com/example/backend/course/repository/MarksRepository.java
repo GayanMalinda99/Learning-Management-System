98ed232d16e5b0320551649ab1b7552c572d73e1
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2022-05-01 21:52:43 +0530

    Update backend

--- backend/backend/src/main/java/com/example/backend/course/repository/MarksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.course.repository;
-
-import com.example.backend.course.model.Marks;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarksRepository extends JpaRepository<Marks, Integer> {
-}
