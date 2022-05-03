15e81aaa1bc22ce40cdd102e858a76ffc989c4dd
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2022-05-03 19:12:02 +0530

    Update StudentCourseController

--- backend/backend/src/main/java/com/example/backend/course/controller/StudentCourseController.java
+++ backend/backend/src/main/java/com/example/backend/course/controller/StudentCourseController.java
@@ -1,12 +1,14 @@
 package com.example.backend.course.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1")
 public class StudentCourseController {
 
-    @GetMapping("api/v1/enrolecourse")
+    @GetMapping("/enrolecourse")
     public String enroleCourse(){
         return "Successfully enrolled";
     }
