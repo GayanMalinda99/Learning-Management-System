2186094bb3329e7545b31d43254ef3f15bbf71ab
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2022-05-03 20:05:32 +0530

    Update CourseEnrolementService Class

--- backend/backend/src/main/java/com/example/backend/service/CourseEnrolementService.java
+++ backend/backend/src/main/java/com/example/backend/service/CourseEnrolementService.java
@@ -1,4 +1,18 @@
 package com.example.backend.service;
 
+import com.example.backend.repository.CourseEnrolementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class CourseEnrolementService {
+
+    @Autowired
+    private CourseEnrolementRepository courseEnrolementRepository;
+    public String saveStudentEnrolledCourse() {
+        courseEnrolementRepository.save();
+    }
 }
