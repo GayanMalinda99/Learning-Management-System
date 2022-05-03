565710f7e18e810ff5d24521ed3a102012de7257
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2022-05-03 19:42:01 +0530

    Create Data Transfer Object package and CourseEnrolmentDTO Class

--- backend/backend/src/main/java/com/example/backend/course/model/CourseEnrolement.java
+++ backend/backend/src/main/java/com/example/backend/course/model/CourseEnrolement.java
@@ -1,11 +1,17 @@
 package com.example.backend.course.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CourseEnrolement {
-
     @Id
     private int studentId;
     private String code;
