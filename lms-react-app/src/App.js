ba986034cbcf380240a905747f1fcf57bbd1c254
SachPrecious <sachithrabandara49@gmail.com>
2022-05-04 19:45:12 +0530

    Login Registration Frontend

--- lms-react-app/src/App.js
+++ lms-react-app/src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import './App.css';
 import AddCourse from './components/AddCourse';
 import {Login} from './components/login';
--- lms-react-app/src/App.jsx
+++ lms-react-app/src/App.jsx
@@ -1,3 +1,18 @@
 import React from "react";
 import "./App.scss";
 import { Login, Register } from "./components/login/index";
+
+
+
+class App extends React.Component{
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          isLogginActive: true
+        };
+      }
+
+
+}
