940c2df282777394d0b7d1e324f7b61c0b8a2183
SachPrecious <sachithrabandara49@gmail.com>
2022-05-04 19:42:23 +0530

    Login Registration Frontend

--- lms-react-app/src/App.js
+++ lms-react-app/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import './App.css';
 import AddCourse from './components/AddCourse';
 import {Login} from './components/login';
---
+++ lms-react-app/src/App.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+import "./App.scss";
+import { Login, Register } from "./components/login/index";
