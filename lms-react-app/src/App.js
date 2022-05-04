68753418937b4dd360f8b47a10c5181ccecb1b4b
SachPrecious <sachithrabandara49@gmail.com>
2022-05-04 16:07:23 +0530

    Login Registration Frontend

--- lms-react-app/src/App.js
+++ lms-react-app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import AddCourse from './components/AddCourse';
+import {Login} from './components/login';
+import {Register} from './components/login';
 
 function App() {
   return (
     <div className="App">
       <AddCourse />
+      
     </div>
   );
 }
---
+++ lms-react-app/src/components/login/login.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import loginImg from "../../login.svg";
+
+export class login extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="base-container">
+        <div className="header">Login</div>
+        <div className="content">
+          <div className="image">
+            <img src="{loginImg}" alt="" srcset="" />
+          </div>
+          <div className="form">
+            <label htmlFor="username">Email</label>
+            <input type="text" name="username" placeholder="User Name" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="text" name="password" placeholder="Password" />
+          </div>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Login
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
