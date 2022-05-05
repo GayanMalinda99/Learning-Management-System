b87c82c6abc7fd84a060e80bf80d016c467bc976
Tharaka-ariyarathna <tharakaariyarathna04@gmail.com>
2022-05-05 12:37:46 +0530

    email sent to dummy array of mails

---
+++ backend/backend/src/main/java/com/example/backend/course/announcements/AnnouncementEmailSender.java
@@ -0,0 +1,23 @@
+package com.example.backend.course.announcements;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnnouncementEmailSender {
+    @Autowired
+    private JavaMailSender mailSender ;
+
+    public void sendAnnouncement(String receiver, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage() ;
+        message.setFrom("elekstoreteam7@gmail.com");
+        message.setTo(receiver);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+        System.out.println("done");
+    }
+}
--- backend/backend/src/main/java/com/example/backend/course/controller/AnnouncementController.java
+++ backend/backend/src/main/java/com/example/backend/course/controller/AnnouncementController.java
@@ -1,15 +1,34 @@
 package com.example.backend.course.controller;
 
+import com.example.backend.course.announcements.AnnouncementEmailSender;
+import com.example.backend.email.EmailSender;
+import com.example.backend.email.EmailService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/v1/")
 public class AnnouncementController {
+    @Autowired
+    private AnnouncementEmailSender announcementEmailSender ;
+
+    /*@EventListener(ApplicationReadyEvent.class)
+    public void sendEmail(String receiver, String subject, String body){
+        announcementEmailSender.sendAnnouncement("1","2","3");
+    }*/
+
+    String[] mailArr = {"tharakaariyarathna04@gmail.com", "lm10@gmail.com", "bigbenz01@gmail.com"} ;
 
     @PostMapping("announcement/{announce}")
     public void getAnnouncement(@PathVariable String announce){
-
+        for(int i = 0 ; i < mailArr.length ; i++){
+            announcementEmailSender.sendAnnouncement(mailArr[i], "Announcement", announce);
+        }
         System.out.println(announce);
     }
 }
