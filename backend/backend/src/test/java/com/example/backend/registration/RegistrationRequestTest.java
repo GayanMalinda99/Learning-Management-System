3120083a702c81f1bbfe49488c026eb656b8b6e7
SachPrecious <sachithrabandara49@gmail.com>
2022-05-07 07:47:36 +0530

    Test-Cases

---
+++ backend/backend/src/test/java/com/example/backend/registration/RegistrationRequestTest.java
@@ -0,0 +1,55 @@
+package com.example.backend.registration;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+class RegistrationRequestTest {
+
+    RegistrationRequest underTestRequest=new RegistrationRequest(
+            "sachithra",
+            "manamperi",
+            "sachithrabandara49@gmail.com",
+            "Hello");
+
+    String expectedFirstName="sachithra";
+    String expectedLastName="manamperi";
+    String expectedEmail="sachithrabandara49@gmail.com";
+    @Test
+    void testEquals() {
+
+    }
+
+    @Test
+    void canEqual() {
+
+    }
+
+    @Test
+    void getFirstName() {
+
+        assertThat(underTestRequest.getFirstName()).isEqualTo(expectedFirstName);
+
+    }
+
+    @Test
+    void getLastName() {
+        assertThat(underTestRequest.getLastName()).isEqualTo(expectedLastName);
+
+    }
+
+    @Test
+    void getEmail() {
+        assertThat(underTestRequest.getEmail()).isEqualTo(expectedEmail);
+
+    }
+
+    @Test
+    @Disabled
+    void getPassword() {
+
+
+    }
+}
