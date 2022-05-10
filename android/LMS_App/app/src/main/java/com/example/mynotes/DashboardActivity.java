160b10fb691030815b065dae461f6b26616ef608
Issiwara Kavinda <issiwaraperera9912@gmail.com>
2022-05-11 00:57:41 +0530

    AppV7

--- android/LMS_App/app/src/main/java/com/example/mynotes/DashboardActivity.java
+++ android/LMS_App/app/src/main/java/com/example/mynotes/DashboardActivity.java
@@ -3,8 +3,15 @@ package com.example.mynotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    EditText etEmail,etPassword,etName,etLname;
+    Button btnRegister;
+
+    final String url_Login =  "http:/" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
