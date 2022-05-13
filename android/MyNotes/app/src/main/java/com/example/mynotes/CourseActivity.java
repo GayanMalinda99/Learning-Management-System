82b6e37ff17edfead5defeee4c6cfd45c7ff28c2
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-05-14 03:16:34 +0530

    Created toolbar in CourseActivity

--- android/MyNotes/app/src/main/java/com/example/mynotes/CourseActivity.java
+++ android/MyNotes/app/src/main/java/com/example/mynotes/CourseActivity.java
@@ -3,12 +3,19 @@ package com.example.mynotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class CourseActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
+
+        androidx.appcompat.widget.Toolbar toolbar=(androidx.appcompat.widget.Toolbar)findViewById(R.id.toolbar2);
+
+        setSupportActionBar(toolbar);
     }
 }
