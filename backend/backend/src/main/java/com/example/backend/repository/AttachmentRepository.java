0f3b2d1b1b8ab84d5f210016cbc3fe71721c8576
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2022-05-17 05:11:59 +0530

    Update AttachmentRepository interface

--- backend/backend/src/main/java/com/example/backend/repository/AttachmentRepository.java
+++ backend/backend/src/main/java/com/example/backend/repository/AttachmentRepository.java
@@ -1,4 +1,9 @@
 package com.example.backend.repository;
 
-public interface AttachmentRepository {
+import com.example.backend.filehandling.Attachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttachmentRepository extends JpaRepository<Attachment, String> {
 }
