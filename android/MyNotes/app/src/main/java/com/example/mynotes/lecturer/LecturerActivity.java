6ed991a20cae2638efa98310c678c0b8a00d1391
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-05-18 01:03:25 +0530

    Craeted activity to create a new course

--- android/MyNotes/app/src/main/java/com/example/mynotes/lecturer/LecturerActivity.java
+++ android/MyNotes/app/src/main/java/com/example/mynotes/lecturer/LecturerActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mynotes.R;
 import com.example.mynotes.adapters.LecturerCourseAdapter;
@@ -35,5 +38,14 @@ public class LecturerActivity extends AppCompatActivity {
                 new LecturerCourseAdapter(this,courses) ;
         recyclerView.setAdapter(lecturerCourseAdapter);
         lecturerCourseAdapter.getItemCount() ;
+
+        Button addCourseButton = (Button)findViewById(R.id.lecturer_add_course_button) ;
+        addCourseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //openNewCourseActivity();
+            }
+        });
     }
+
 }
---
+++ android/MyNotes/app/src/main/java/com/example/mynotes/lecturer/NewCourseActivity.java
@@ -0,0 +1,16 @@
+package com.example.mynotes.lecturer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mynotes.R;
+
+public class NewCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_course);
+    }
+}
