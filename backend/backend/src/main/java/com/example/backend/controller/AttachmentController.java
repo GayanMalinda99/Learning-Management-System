eb117807745210f944143ec17b661394581b2e9d
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2022-05-17 05:36:21 +0530

    Update AttachmentController

--- backend/backend/src/main/java/com/example/backend/controller/AttachmentController.java
+++ backend/backend/src/main/java/com/example/backend/controller/AttachmentController.java
@@ -1,7 +1,14 @@
 package com.example.backend.controller;
 
+import com.example.backend.service.AttachmentService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AttachmentController {
+
+    private AttachmentService attachmentService;
+
+    public AttachmentController(AttachmentService attachmentService) {
+        this.attachmentService = attachmentService;
+    }
 }
