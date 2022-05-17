6d93158270ba6d7dc1ff08a7d031c28023cd1344
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2022-05-17 05:50:21 +0530

    Update AttachmentserviceImpl

--- backend/backend/src/main/java/com/example/backend/service/AttachmentServiceImpl.java
+++ backend/backend/src/main/java/com/example/backend/service/AttachmentServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.backend.service;
 import com.example.backend.filehandling.Attachment;
 import com.example.backend.repository.AttachmentRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -16,6 +17,11 @@ public class AttachmentServiceImpl implements AttachmentService{
 
     @Override
     public Attachment saveAttachment(MultipartFile file) {
-        return null;
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+
+        } catch {
+            
+        }
     }
 }
