c6adad3931b3e7a70b54a95c5b1bec44fa597731
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-05-17 23:32:27 +0530

    Created a xml file to create item for lecturers conducting course list

--- android/MyNotes/app/src/main/java/com/example/mynotes/lecturer/LecturerActivity.java
+++ android/MyNotes/app/src/main/java/com/example/mynotes/lecturer/LecturerActivity.java
@@ -1,16 +1,33 @@
 package com.example.mynotes.lecturer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.mynotes.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LecturerActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecturer);
+
+        recyclerView = (RecyclerView)findViewById(R.id.lecturer_recycleview) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<String> courses = new ArrayList<>() ;
+        courses.add("Web Development") ;
+        courses.add("Mobile Development") ;
+        courses.add("Software Architecture") ;
+        courses.add("Descrete Mathematics") ;
+        courses.add("Software Validation") ;
     }
 }
