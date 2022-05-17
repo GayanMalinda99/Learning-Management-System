1052f4f79af08a85c6bfcaacea9a9dfbb8043a86
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2022-05-17 05:41:15 +0530

    Update AttachmentController with uploadFile function

--- backend/backend/src/main/java/com/example/backend/controller/AttachmentController.java
+++ backend/backend/src/main/java/com/example/backend/controller/AttachmentController.java
@@ -1,7 +1,12 @@
 package com.example.backend.controller;
 
+import com.example.backend.filehandling.Attachment;
+import com.example.backend.filehandling.model.ResponseData;
 import com.example.backend.service.AttachmentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class AttachmentController {
@@ -10,5 +15,10 @@ public class AttachmentController {
 
     public AttachmentController(AttachmentService attachmentService) {
         this.attachmentService = attachmentService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseData uploadFile(@RequestPart("file")MultipartFile file) {
+        Attachment attachment = null;
     }
 }
