36e8ba1df800b99142ed2fefcff3974ed7192814
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-05-18 02:31:13 +0530

    Made submit_button in new course activity clickable

--- android/MyNotes/app/src/main/java/com/example/mynotes/lecturer/NewCourseActivity.java
+++ android/MyNotes/app/src/main/java/com/example/mynotes/lecturer/NewCourseActivity.java
@@ -3,6 +3,10 @@ package com.example.mynotes.lecturer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.mynotes.R;
 
@@ -12,5 +16,18 @@ public class NewCourseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_course);
+
+        EditText code = findViewById(R.id.input_code) ;
+        EditText title = findViewById(R.id.input_title) ;
+        EditText discription = findViewById(R.id.input_description) ;
+        Button submitButton = findViewById(R.id.submit_course_button) ;
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("messege", code.getText().toString()+" / "+
+                        title.getText().toString()+" / "+discription.getText().toString()) ;
+            }
+        });
     }
 }
