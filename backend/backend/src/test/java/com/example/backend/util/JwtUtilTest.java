e6a566ca603aeb672104e399397b4c126803f2f0
SachPrecious <sachithrabandara49@gmail.com>
2022-05-19 20:48:02 +0530

    test

---
+++ backend/backend/src/test/java/com/example/backend/util/JwtUtilTest.java
@@ -0,0 +1,38 @@
+package com.example.backend.util;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtUtilTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getUsernameFromToken() {
+    }
+
+    @Test
+    void getClaimFromToken() {
+    }
+
+    @Test
+    void validateToken() {
+    }
+
+    @Test
+    void getExpirationDateFromToken() {
+    }
+
+    @Test
+    void generateToken() {
+    }
+}
