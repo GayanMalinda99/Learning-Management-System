656ed7f7f760be49e6a88cf67f4c045158d9de63
SachPrecious <sachithrabandara49@gmail.com>
2022-05-19 20:36:41 +0530

    test

---
+++ backend/backend/src/test/java/com/example/backend/controller/AttachmentControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.backend.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AttachmentControllerTest {
+
+    @Test
+    void uploadFile() {
+    }
+
+    @Test
+    void downloadFile() {
+    }
+}
