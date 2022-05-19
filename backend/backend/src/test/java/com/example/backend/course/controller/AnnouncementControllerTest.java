8d6426ca7aa9d58498175073ff2e1273f0fa9d88
SachPrecious <sachithrabandara49@gmail.com>
2022-05-19 20:13:25 +0530

    test-

---
+++ backend/backend/src/test/java/com/example/backend/course/controller/AnnouncementControllerTest.java
@@ -0,0 +1,69 @@
+package com.example.backend.course.controller;
+
+import com.example.backend.course.repository.StudentEnrollRepository;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ContextConfiguration(classes = {AnnouncementController.class})
+@ExtendWith(SpringExtension.class)
+class AnnouncementControllerTest {
+
+    @Autowired
+    private AnnouncementController announcementController;
+
+    @MockBean
+    private StudentEnrollRepository studentEnrollRepository;
+
+    @Test
+    void getAnnouncement() {
+    }
+
+    @Test
+    void getEnrollStudent() {
+    }
+
+    /**
+     * Method under test: {@link AnnouncementController#getEnrollStudent()}
+     */
+    @Test
+    void testGetEnrollStudent() throws Exception {
+        when(this.studentEnrollRepository.findAll()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/v1/announcement");
+        MockMvcBuilders.standaloneSetup(this.announcementController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    /**
+     * Method under test: {@link AnnouncementController#getEnrollStudent()}
+     */
+    @Test
+    void testGetEnrollStudent2() throws Exception {
+        when(this.studentEnrollRepository.findAll()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/api/v1/announcement");
+        getResult.contentType("https://example.org/example");
+        MockMvcBuilders.standaloneSetup(this.announcementController)
+                .build()
+                .perform(getResult)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+}
---
+++ backend/backend/src/test/java/com/example/backend/registration/RegistrationServiceTest.java
@@ -0,0 +1,178 @@
+package com.example.backend.registration;
+
+import com.example.backend.appuser.AppUser;
+import com.example.backend.appuser.AppUserRole;
+import com.example.backend.appuser.AppUserService;
+import com.example.backend.email.EmailSender;
+import com.example.backend.registration.token.ConfirmationToken;
+import com.example.backend.registration.token.ConfirmationTokenService;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ContextConfiguration(classes = {RegistrationService.class})
+@ExtendWith(SpringExtension.class)
+class RegistrationServiceTest {
+
+    @MockBean
+    private AppUserService appUserService;
+
+    @MockBean
+    private ConfirmationTokenService confirmationTokenService;
+
+    @MockBean
+    private EmailSender emailSender;
+
+    @MockBean
+    private EmailValidator emailValidator;
+
+    @Autowired
+    private RegistrationService registrationService;
+
+    @Test
+    void register() {
+    }
+
+    /**
+     * Method under test: {@link RegistrationService#confirmToken(String)}
+     */
+    @Test
+    void testConfirmToken() {
+        AppUser appUser = new AppUser();
+        appUser.setAppUserRole(AppUserRole.STUDENT);
+        appUser.setCourse(new HashSet<>());
+        appUser.setCourses(new HashSet<>());
+        appUser.setEmail("jane.doe@example.org");
+        appUser.setEnabled(true);
+        appUser.setFirstName("Jane");
+        appUser.setId(123L);
+        appUser.setLastName("Doe");
+        appUser.setLocked(true);
+        appUser.setMarks(new HashSet<>());
+        appUser.setPassword("iloveyou");
+
+        ConfirmationToken confirmationToken = new ConfirmationToken();
+        confirmationToken.setAppUser(appUser);
+        confirmationToken.setConfirmedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        confirmationToken.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        confirmationToken.setExpiredAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        confirmationToken.setId(123L);
+        confirmationToken.setToken("ABC123");
+        Optional<ConfirmationToken> ofResult = Optional.of(confirmationToken);
+        when(this.confirmationTokenService.getToken((String) any())).thenReturn(ofResult);
+        assertThrows(IllegalStateException.class, () -> this.registrationService.confirmToken("ABC123"));
+        verify(this.confirmationTokenService).getToken((String) any());
+    }
+
+    /**
+     * Method under test: {@link RegistrationService#confirmToken(String)}
+     */
+    @Test
+    void testConfirmToken2() {
+        AppUser appUser = new AppUser();
+        appUser.setAppUserRole(AppUserRole.STUDENT);
+        appUser.setCourse(new HashSet<>());
+        appUser.setCourses(new HashSet<>());
+        appUser.setEmail("jane.doe@example.org");
+        appUser.setEnabled(true);
+        appUser.setFirstName("Jane");
+        appUser.setId(123L);
+        appUser.setLastName("Doe");
+        appUser.setLocked(true);
+        appUser.setMarks(new HashSet<>());
+        appUser.setPassword("iloveyou");
+        ConfirmationToken confirmationToken = mock(ConfirmationToken.class);
+        when(confirmationToken.getConfirmedAt()).thenReturn(null);
+        when(confirmationToken.getExpiredAt()).thenReturn(LocalDateTime.of(1, 1, 1, 1, 1));
+        doNothing().when(confirmationToken).setAppUser((AppUser) any());
+        doNothing().when(confirmationToken).setConfirmedAt((LocalDateTime) any());
+        doNothing().when(confirmationToken).setCreatedAt((LocalDateTime) any());
+        doNothing().when(confirmationToken).setExpiredAt((LocalDateTime) any());
+        doNothing().when(confirmationToken).setId((Long) any());
+        doNothing().when(confirmationToken).setToken((String) any());
+        confirmationToken.setAppUser(appUser);
+        confirmationToken.setConfirmedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        confirmationToken.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        confirmationToken.setExpiredAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        confirmationToken.setId(123L);
+        confirmationToken.setToken("ABC123");
+        Optional<ConfirmationToken> ofResult = Optional.of(confirmationToken);
+        when(this.confirmationTokenService.getToken((String) any())).thenReturn(ofResult);
+        assertThrows(IllegalStateException.class, () -> this.registrationService.confirmToken("ABC123"));
+        verify(this.confirmationTokenService).getToken((String) any());
+        verify(confirmationToken).getConfirmedAt();
+        verify(confirmationToken).getExpiredAt();
+        verify(confirmationToken).setAppUser((AppUser) any());
+        verify(confirmationToken).setConfirmedAt((LocalDateTime) any());
+        verify(confirmationToken).setCreatedAt((LocalDateTime) any());
+        verify(confirmationToken).setExpiredAt((LocalDateTime) any());
+        verify(confirmationToken).setId((Long) any());
+        verify(confirmationToken).setToken((String) any());
+    }
+
+    /**
+     * Method under test: {@link RegistrationService#confirmToken(String)}
+     */
+    @Test
+    void testConfirmToken3() {
+        when(this.confirmationTokenService.getToken((String) any())).thenReturn(Optional.empty());
+
+        AppUser appUser = new AppUser();
+        appUser.setAppUserRole(AppUserRole.STUDENT);
+        appUser.setCourse(new HashSet<>());
+        appUser.setCourses(new HashSet<>());
+        appUser.setEmail("jane.doe@example.org");
+        appUser.setEnabled(true);
+        appUser.setFirstName("Jane");
+        appUser.setId(123L);
+        appUser.setLastName("Doe");
+        appUser.setLocked(true);
+        appUser.setMarks(new HashSet<>());
+        appUser.setPassword("iloveyou");
+        ConfirmationToken confirmationToken = mock(ConfirmationToken.class);
+        when(confirmationToken.getConfirmedAt()).thenReturn(LocalDateTime.of(1, 1, 1, 1, 1));
+        when(confirmationToken.getExpiredAt()).thenReturn(LocalDateTime.of(1, 1, 1, 1, 1));
+        doNothing().when(confirmationToken).setAppUser((AppUser) any());
+        doNothing().when(confirmationToken).setConfirmedAt((LocalDateTime) any());
+        doNothing().when(confirmationToken).setCreatedAt((LocalDateTime) any());
+        doNothing().when(confirmationToken).setExpiredAt((LocalDateTime) any());
+        doNothing().when(confirmationToken).setId((Long) any());
+        doNothing().when(confirmationToken).setToken((String) any());
+        confirmationToken.setAppUser(appUser);
+        confirmationToken.setConfirmedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        confirmationToken.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        confirmationToken.setExpiredAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        confirmationToken.setId(123L);
+        confirmationToken.setToken("ABC123");
+        assertThrows(IllegalStateException.class, () -> this.registrationService.confirmToken("ABC123"));
+        verify(this.confirmationTokenService).getToken((String) any());
+        verify(confirmationToken).setAppUser((AppUser) any());
+        verify(confirmationToken).setConfirmedAt((LocalDateTime) any());
+        verify(confirmationToken).setCreatedAt((LocalDateTime) any());
+        verify(confirmationToken).setExpiredAt((LocalDateTime) any());
+        verify(confirmationToken).setId((Long) any());
+        verify(confirmationToken).setToken((String) any());
+    }
+
+    @Test
+    void confirmToken() {
+    }
+}
