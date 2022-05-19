18f1693b0f01e6d8ee50546425c64e5b2fc9cfac
SachPrecious <sachithrabandara49@gmail.com>
2022-05-19 20:46:42 +0530

    test

--- backend/backend/src/test/java/com/example/backend/security/PasswordEncoderTest.java
+++ backend/backend/src/test/java/com/example/backend/security/PasswordEncoderTest.java
@@ -1,5 +1,7 @@
 package com.example.backend.security;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,18 @@ class PasswordEncoderTest {
 
 
         this.passwordEncoder.bCryptPasswordEncoder();
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void bCryptPasswordEncoder() {
     }
 }
 
