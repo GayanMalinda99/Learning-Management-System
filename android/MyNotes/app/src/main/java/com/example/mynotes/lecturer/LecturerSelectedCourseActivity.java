88fd67f3baac46033d33e072c9f7886f27df2395
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-05-20 17:27:22 +0530

    Added functions to see marks

--- android/MyNotes/app/src/main/java/com/example/mynotes/lecturer/LecturerSelectedCourseActivity.java
+++ android/MyNotes/app/src/main/java/com/example/mynotes/lecturer/LecturerSelectedCourseActivity.java
@@ -3,6 +3,7 @@ package com.example.mynotes.lecturer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.mynotes.R;
 
@@ -12,5 +13,10 @@ public class LecturerSelectedCourseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecturer_selected_course);
+
+        Button addMarksButton  = findViewById(R.id.add_marks_button) ;
+        Button announcementButton = findViewById(R.id.announcement_button) ;
+
+
     }
 }
