4cb3a6f3f0d49ff586ecedc53bc6eb54e8d657e9
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-05-20 17:35:32 +0530

    create navigation between selected course and adding marks activity

--- android/MyNotes/app/src/main/java/com/example/mynotes/lecturer/LecturerSelectedCourseActivity.java
+++ android/MyNotes/app/src/main/java/com/example/mynotes/lecturer/LecturerSelectedCourseActivity.java
@@ -2,7 +2,9 @@ package com.example.mynotes.lecturer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.mynotes.R;
@@ -17,6 +19,20 @@ public class LecturerSelectedCourseActivity extends AppCompatActivity {
         Button addMarksButton  = findViewById(R.id.add_marks_button) ;
         Button announcementButton = findViewById(R.id.announcement_button) ;
 
+        addMarksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddMarksActivity.class) ;
+                startActivity(intent);
+            }
+        });
 
+        announcementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AnnouncementActivity.class) ;
+                startActivity(intent);
+            }
+        });
     }
 }
