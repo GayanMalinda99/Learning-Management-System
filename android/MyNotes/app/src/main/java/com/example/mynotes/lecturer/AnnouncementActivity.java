9e28a77d2b67253d8328fa57c6fa07c79a4ef263
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-05-22 12:46:11 +0530

    Created functions to send an announcemet

--- android/MyNotes/app/src/main/java/com/example/mynotes/lecturer/AnnouncementActivity.java
+++ android/MyNotes/app/src/main/java/com/example/mynotes/lecturer/AnnouncementActivity.java
@@ -3,12 +3,17 @@ package com.example.mynotes.lecturer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.mynotes.R;
 import com.example.mynotes.retrofit.CoursesApi;
 import com.example.mynotes.retrofit.RetrofitClientInstance;
 
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
@@ -19,9 +24,31 @@ public class AnnouncementActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_announcement);
 
-        Retrofit retrofit = new RetrofitClientInstance().getRetrofitInstance() ;
-        final CoursesApi api = retrofit.create(CoursesApi.class) ;
+        EditText announcementEdittext = findViewById(R.id.textArea_announcement) ;
+        Button sendButton = findViewById(R.id.announcement_button) ;
 
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String announcement = announcementEdittext.getText().toString() ;
+
+                Retrofit retrofit = new RetrofitClientInstance().getRetrofitInstance() ;
+                final CoursesApi api = retrofit.create(CoursesApi.class) ;
+
+                Call<Response> call = api.announce(announcement) ;
+                call.enqueue(new Callback<Response>() {
+                    @Override
+                    public void onResponse(Call<Response> call, Response<Response> response) {
+                        Log.i("Messege", "Done") ;
+                    }
+
+                    @Override
+                    public void onFailure(Call<Response> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
 
     }
 }
