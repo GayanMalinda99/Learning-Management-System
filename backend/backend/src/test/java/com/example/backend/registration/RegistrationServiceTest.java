b789abb9813b1200e52669c5f8e982984937c166
ISSIWARA KAVINDA <issiwaraperera9912@gmail.com>
2022-05-22 17:42:05 +0530

    tokenconfirmation

---
+++ backend/backend/src/test/java/com/example/backend/registration/RegistrationServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.backend.registration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void confirmToken() {
+    }
+}
